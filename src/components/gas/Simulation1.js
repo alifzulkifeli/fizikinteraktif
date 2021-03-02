a9aadf55fa314aefa2c1c3d5cf2178d6340e6c0d
aliff <alifzulkifeli@gmail.com>
2021-03-02 17:51:26 +0900

    sad

--- src/components/gas/Simulation1.js
+++ src/components/gas/Simulation1.js
@@ -5,6 +5,7 @@ export default function Simulation1() {
     <>
       <h2 h2 className="sm:block md:hidden text-center text-2xl font-extrabold font-monospace  text-red-700 p-3 py-9 mb-7 || md:p-10" > Buka laman web menggunakan desktop untuk mengakses simulasi</h2 >
       <embed className=" mt-10 hidden md:block w-full h-full h-74 " src="https://ch301.cm.utexas.edu/simulations/js/idealgaslaw/" type="" />
+
     </>
   )
 }
