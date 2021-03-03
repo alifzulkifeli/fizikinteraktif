6bdbbd4f8c6be68773023540ebf1d7ad548c4e3b
aliff <alifzulkifeli@gmail.com>
2021-03-04 05:56:47 +0900

    ready for deploy3

--- src/components/gas/Note1.js
+++ src/components/gas/Note1.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import AllPages from '../allPages';
+import SinglePage from '../singlePage';
+import note1 from "./nota1.pdf"
 export default function Note1() {
+  
   return (
-    <object data="nota1.pdf" type="application/pdf" className="h-screen w-screen">
- 
-</object> 
+    <div >
+    <h4>Single Page</h4>
+    <SinglePage pdf={note1} />
+
+    <hr />
+
+    <h4>All Pages</h4>
+    <div className="all-page-container">
+      <AllPages pdf={note1} />
+    </div>
+
+    <hr />
+  </div>
   );
 }
 
