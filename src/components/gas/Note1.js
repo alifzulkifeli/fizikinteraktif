234975f18e0a2b98795ab01cb395d0302833c30f
aliff <alifzulkifeli@gmail.com>
2021-03-04 22:45:53 +0900

    resize pdf

--- src/components/gas/Note1.js
+++ src/components/gas/Note1.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SinglePage from '../singlePage';
-import note1 from "./nota1.pdf"
+import note1 from "./note1.pdf"
 export default function Note1() {
   
   return (
